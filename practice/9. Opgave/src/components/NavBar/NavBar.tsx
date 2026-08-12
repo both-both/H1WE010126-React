9b5f05884f117c83c76ed32da9114e8e54209b5c
both-both <claraboth@gmail.com>
2026-08-12 13:51:21 +0200

    9. Opgave: Opsæt CSS modules til dine komponenter

---
+++ practice/9. Opgave/src/components/NavBar/NavBar.tsx
@@ -0,0 +1,14 @@
+export const NavBar = () => {
+  return (
+    <nav className="{styles.navbar}">
+      <ul>
+        <li>Verdensmålene</li>
+        <li>Undervisning</li>
+        <li>Byg dit eget mål</li>
+        <li>FAQ</li>
+        <li>Kontakt os</li>
+        <li>Links</li>
+      </ul>
+    </nav>
+  );
+};
